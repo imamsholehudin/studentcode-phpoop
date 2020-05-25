84ce230c89d3c9b855b1b7d1b072cc69179190da
imamsholehudin <sholehudinimam@gmail.com>
2020-05-25 19:43:27 +0700

    upload ke-3

---
+++ video16/App/index.php
@@ -0,0 +1,25 @@
+<?php 
+
+
+require_once 'init.php';
+// require_once 'Produk/InfoProduk.php';
+// require_once 'Produk/Produk.php';
+// require_once 'Produk/Komik.php';
+// require_once 'Produk/Game.php';
+// require_once 'Produk/CetakInfoProduk.php';
+// require_once 'App/Produk/InfoProduk.php';
+// require_once 'App/Produk/Produk.php';
+// require_once 'App/Produk/Komik.php';
+// require_once 'App/Produk/Game.php';
+// require_once 'App/Produk/CetakInfoProduk.php';
+
+
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 50);
+
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk( $produk1 );
+$cetakProduk->tambahProduk( $produk2 );
+echo $cetakProduk->cetak();
+
+ ?>
---
+++ video16/App/init.php
@@ -0,0 +1,3 @@
+<?php spl_autoload_register(function( $class ){
+    require_once __DIR__ . '/Produk/' . $class . '.php';
+}); ?>
